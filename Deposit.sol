c9039e9ca562f41e06de85c05232cd451996fd0d
19IT117 <19it117@charusat.edu.in>
2022-07-25 16:43:23 +0530

    updating Deposit Contract

--- Deposit.sol
+++ Deposit.sol
@@ -7,15 +7,12 @@ contract Deposit{
     
     SSTestToken token1;
 
-
     address public owner;
-    uint32 public TVLocked;
-    uint32 public ownerBalance;
-    uint32 public interestPayable;
+    uint32 public TVLocked;  
     uint32 public ownerEarnings;
     uint32 public contractReserve;
     uint32 public TVLoaned;
-    
+
     struct LockDetails {
         uint32 amount;
         uint256 lockperiod;
@@ -26,37 +23,38 @@ contract Deposit{
         uint256 timestamp;
     }
 
+
     mapping(address => LockDetails[]) public LockMapping;
     mapping(address => LoanDetails[]) public LoanMapping;
-    
+
     constructor(address _contractaddress1){
+        
         token1 = SSTestToken(_contractaddress1);
         owner = msg.sender;
         //t.transferFrom(owner,address(this),1000000);
     }
 
     function DepositToken(uint32 amount , uint256 timeperiod) external{
-        require(amount>=1000,"Deposit more than 1000");
-        require(timeperiod>=10 , "Need to deposit more than 10 Seconds");
-        token1.transferFrom(msg.sender,address(this),amount);
-        LockMapping[msg.sender].push(LockDetails({amount : amount , lockperiod : block.timestamp + timeperiod}));
+        address depositor = msg.sender;
+        require(amount>=1000,"Deposit more");
+        require(timeperiod>=10 , "Increase deposit time");
+        token1.transferFrom(depositor,owner,amount);
+        LockMapping[depositor].push(LockDetails({amount : amount , lockperiod : block.timestamp + timeperiod}));
         unchecked {
             TVLocked += amount;
-            interestPayable += (amount * 10)/100;
         }
-        require(ownerBalance>interestPayable, "Sorry Deposit can't be done please try later.");
+        
     }
 
     function ClaimTokens(uint256 index) external{
-        require(LockMapping[msg.sender][index].lockperiod<=block.timestamp, "you can't withdraw now");
-        LockDetails storage s =  LockMapping[msg.sender][index];
+        address depositor = msg.sender;
+        require(LockMapping[depositor][index].lockperiod<=block.timestamp , "you can't withdraw now");
+        LockDetails storage s =  LockMapping[depositor][index];
         unchecked{
-            TVLocked -= s.amount;
-            interestPayable -= (s.amount * 10)/100;
-            ownerBalance -= (s.amount * 10)/100;    
+            TVLocked -= s.amount;    
         }
-        token1.transfer(msg.sender,(s.amount*11)/10);
-        delete LockMapping[msg.sender][index];
+        token1.transferFrom(owner,depositor,(s.amount*110)/10);
+        delete LockMapping[depositor][index];
     }
 
     modifier onlyOwner{
@@ -65,27 +63,17 @@ contract Deposit{
     }
 
     function changeOwnership(address newOwner) external onlyOwner{
-        token1.transferFrom(newOwner,address(this),ownerBalance);
+        require(token1.allowance(newOwner,address(this))>0,"Owner need to allow transferfrom");
         owner = newOwner;
-        token1.transfer(owner,ownerBalance);
     }
 
-    function ownerDeposit(uint32 amount) external onlyOwner{
-        token1.transferFrom(owner,address(this),amount);
-        ownerBalance += amount;
-    }
-
-    function revokeDeposit(uint32 amount) external onlyOwner{
-        require(amount<interestPayable , "Deposit can cause trouble for the project");
-        token1.transfer(owner,amount);
-        ownerBalance -= amount;
-    }
 
     function takeLoan() public payable{
+        address loanTaker = msg.sender;
         uint32 amount = uint32(msg.value);
-        require(TVLocked-TVLoaned <= amount, "Insufficent fund in the contract");
-        LoanMapping[msg.sender].push(LoanDetails({amount : amount , timestamp : block.timestamp}));
-        token1.transfer(msg.sender,msg.value);
+        require(TVLocked-TVLoaned <= amount && token1.allowance(owner,address(this))>= amount, "Insufficent fund in the contract");
+        LoanMapping[loanTaker].push(LoanDetails({amount : amount , timestamp : block.timestamp}));
+        token1.transfer(loanTaker,msg.value);
         unchecked{
             TVLoaned += amount;
             TVLocked -= amount ;
@@ -93,19 +81,21 @@ contract Deposit{
     }
 
     function payLoan(uint index,uint32 amount) public payable{
-        LoanDetails storage s =LoanMapping[msg.sender][index];
+        address payer =msg.sender;
+        LoanDetails storage s = LoanMapping[payer][index];
         uint32  _amount = s.amount;
         require(amount == (_amount*112)/100,"You need to pay entire loan");
-        require(block.timestamp<=s.timestamp+100, "You are late");
-        token1.transferFrom(msg.sender,address(this),amount);
-        payable(msg.sender).transfer(_amount);
+        require(block.timestamp<=s.timestamp +100, "You are late");
+        token1.transferFrom(payer,address(this),amount);
+        payable(payer).transfer(_amount);
         unchecked{
             TVLoaned -= _amount;
             TVLocked += _amount;
+
             ownerEarnings += (_amount*11)/100;
             contractReserve += (_amount*1)/100;
         }
-        delete LoanMapping[msg.sender][index];
+        delete LoanMapping[payer][index];
     }
 
     function withdrawEarnings() public onlyOwner{
@@ -113,9 +103,22 @@ contract Deposit{
         token1.transfer(owner,ownerEarnings);
         ownerEarnings=0;
     }
+
     function balanceEth() public view returns (uint256){
         return address(this).balance;
     }
     
+    function withdrawToken() public payable onlyOwner{
+        token1.approve(owner,token1.balanceOf(address(this)));
+    }
+
+    function withdrawEth() public payable onlyOwner{
+        payable(owner).transfer(balanceEth());
+    }
+
+    function fallback() external payable{
+
+    }
+
 }
 
